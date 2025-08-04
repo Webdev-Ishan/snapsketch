a9afbb3a760ea3e5fb0e7a1307bbb369a3c0b525
Ishan Saini <ishansaini0105@gmail.com>
2025-08-04 17:16:00 +0530

    The cors issue is fixed

--- snapsketch/apps/http-backend/src/index.ts
+++ snapsketch/apps/http-backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://snapsketch.vercel.app",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
